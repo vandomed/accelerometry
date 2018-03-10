134f40cd87a7bfc1fa24777bb4d94d63fbe6533b
Dane Van Domelen <vandomed@gmail.com>
2018-03-09 20:23:04 -0500

    First push, just artifacts function and datasets.

---
+++ R/accelerometry-data.R
@@ -0,0 +1,15 @@
+#' Uniaxial Sample Data
+#' 
+#' Toy dataset with uniaxial minute-to-minute counts. 
+#' 
+#' @name unidata
+#' @docType data
+NULL
+#'
+#' Triaxial Sample Data
+#' 
+#' Toy dataset with triaxial minute-to-minute counts.
+#' 
+#' @name tridata
+#' @docType data
+NULL
---
+++ src/artifacts.cpp
@@ -0,0 +1,82 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' Accelerometer Artifact Correction
+//'
+//' This function corrects abnormally high count values in minute-to-minute 
+//' accelerometer data by replacing such values with the average of the 
+//' neighboring count values. An integer vector is returned despite the average 
+//' calculation often producing a decimal. This follows the convention used by 
+//' the NCI's SAS programs 
+//' (\url{http://riskfactor.cancer.gov/tools/nhanes_pam}). 
+//' 
+//' @param counts Integer vector with accelerometer count values.
+//' @param thresh Integer value specifying the smallest count value that should 
+//' be considered an artifact.
+//'
+//'
+//' @return Integer vector with artifacts corrected.
+//' 
+//' 
+//' @references 
+//' National Cancer Institute. Risk factor monitoring and methods: SAS programs 
+//' for analyzing NHANES 2003-2004 accelerometer data. Available at: 
+//' \url{http://riskfactor.cancer.gov/tools/nhanes_pam}. Accessed March 9, 2018.
+//' 
+//' Acknowledgment: This material is based upon work supported by the National 
+//' Science Foundation Graduate Research Fellowship under Grant No. DGE-0940903.
+//'
+//'
+//' @examples
+//' # Load toy dataset
+//' data(unidata)
+//' 
+//' # Get data from ID number 21007
+//' counts.part3 <- unidata[unidata[, "seqn"] == 21007, "paxinten"]
+//' 
+//' # Replace counts > 10,000 with average of neighboring values
+//' counts.part3.corrected <- artifacts(counts = counts.part3, thresh = 10000)
+//'
+//'@export
+// [[Rcpp::export]]
+IntegerVector artifacts(IntegerVector counts, int thresh) {
+  int n = counts.size();
+  IntegerVector out(n);
+  int before = -1;
+  int after = -1;
+  if (counts[0] >= thresh) {
+    for (int a = 1; a < n; ++a) {
+      if (counts[a] < thresh) {
+        out[0] = counts[a];
+        break;
+      }
+    }
+  }
+  else out[0] = counts[0];
+  for (int b = 1; b < n; ++b) {
+    before = -1;
+    after = -1;
+    if (counts[b] >= thresh) {
+      for (int c = b - 1; c >= 0; --c) {
+        if (counts[c] < thresh) {
+          before = counts[c];
+          break;
+        }
+      }
+      for (int d = b + 1; d < n; ++d) {
+        if (counts[d] < thresh) {
+          after = counts[d];
+          break;
+        }
+      }
+      if (before > -1 && after > -1) {
+        if (fmod(before + after, 2) == 0) out[b] = (before + after) / 2;
+        else out[b] = (before + after + 1) / 2;
+      }
+      else if (before == -1) out[b] = after;
+      else if (after == -1) out[b] = before;
+    }
+    else out[b] = counts[b];
+  }
+  return(out);
+}
