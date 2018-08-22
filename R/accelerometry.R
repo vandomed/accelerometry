4da3ea076e9654ad630b122866eba3e1f44d179b
Dane Van Domelen <vandomed@gmail.com>
2018-08-22 09:46:17 -0400

    Trigger Travis CI

--- R/accelerometry.R
+++ R/accelerometry.R
@@ -10,7 +10,7 @@
 #' \tabular{ll}{
 #' Package: \tab accelerometry \cr
 #' Type: \tab Package \cr
-#' Version: \tab 3.1.2 \cr
+#' Version: \tab 3.1.1 \cr
 #' Date: \tab 2018-08-22 \cr
 #' License: \tab GPL-3 \cr
 #' }
