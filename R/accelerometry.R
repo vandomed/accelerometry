c62f296c529b4f4e8fd9853cc7cf7ea36ba8eb28
Dane Van Domelen <vandomed@gmail.com>
2018-08-22 12:01:35 -0400

    Back to linux only

--- R/accelerometry.R
+++ R/accelerometry.R
@@ -11,7 +11,7 @@
 #' Package: \tab accelerometry \cr
 #' Type: \tab Package \cr
 #' Version: \tab 3.1.2 \cr
-#' Date: \tab 2018-08-21 \cr
+#' Date: \tab 2018-08-22 \cr
 #' License: \tab GPL-3 \cr
 #' }
 #'
